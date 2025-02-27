5827bb771398c99c84796b1360422170251f6d33
adriycn <faminialmiguel02@gmail.com>
2025-02-27 10:10:50 +0800

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,38 +1,52 @@
-(function(){
-    window.Input = {
-      up: false,
-      down: false
-    };
-  
-    function keyDownHandler(e) {
-      if (e.code === "ArrowUp" || e.code === "Space") {
-        window.Input.up = true;
-        e.preventDefault();
-      }
-      if (e.code === "ArrowDown") {
-        window.Input.down = true;
-        e.preventDefault();
-      }
+document.addEventListener("DOMContentLoaded", function() {
+  window.Input = {
+    up: false,
+    down: false
+  };
+
+  function keyDownHandler(e) {
+    if (e.code === "ArrowUp" || e.code === "Space") {
+      window.Input.up = true;
+      e.preventDefault();
     }
-    function keyUpHandler(e) {
-      if (e.code === "ArrowUp" || e.code === "Space") {
-        window.Input.up = false;
-        e.preventDefault();
-      }
-      if (e.code === "ArrowDown") {
-        window.Input.down = false;
-        e.preventDefault();
-      }
+    if (e.code === "ArrowDown") {
+      window.Input.down = true;
+      e.preventDefault();
     }
-  
-    window.addEventListener("keydown", keyDownHandler, false);
-    window.addEventListener("keyup", keyUpHandler, false);
-  
-    // Mobile Touch: Only Jump button is used.
-    const jumpBtn = document.getElementById("jump-btn");
-    if(jumpBtn){
-      jumpBtn.addEventListener("touchstart", (e) => { window.Input.up = true; e.preventDefault(); });
-      jumpBtn.addEventListener("touchend", (e) => { window.Input.up = false; e.preventDefault(); });
+  }
+  function keyUpHandler(e) {
+    if (e.code === "ArrowUp" || e.code === "Space") {
+      window.Input.up = false;
+      e.preventDefault();
     }
-  })();
-  
+    if (e.code === "ArrowDown") {
+      window.Input.down = false;
+      e.preventDefault();
+    }
+  }
+
+  window.addEventListener("keydown", keyDownHandler, false);
+  window.addEventListener("keyup", keyUpHandler, false);
+
+  const jumpBtn = document.getElementById("jump-btn");
+  if(jumpBtn) {
+    jumpBtn.addEventListener("touchstart", function(e) {
+      window.Input.up = true;
+      e.preventDefault();
+    }, false);
+    jumpBtn.addEventListener("touchend", function(e) {
+      window.Input.up = false;
+      e.preventDefault();
+    }, false);
+    jumpBtn.addEventListener("pointerdown", function(e) {
+      window.Input.up = true;
+      e.preventDefault();
+    }, false);
+    jumpBtn.addEventListener("pointerup", function(e) {
+      window.Input.up = false;
+      e.preventDefault();
+    }, false);
+  } else {
+    console.error("Jump button not found");
+  }
+});
